3e09cc747157396f08aa9379135eebb661cdcb00
Izapicaro <34921842+Izapicaro@users.noreply.github.com>
2018-05-21 16:43:11 +0100

    Update Composite Testing.c

--- testing/Composite Testing.c
+++ testing/Composite Testing.c
@@ -3,30 +3,45 @@
 
 task main(){
 	
+	//direction
 	int x = 1;
-
+	//determines whether controls can be reversed
+	int allowChange = 1;
+	
 	Robot ROBOT;
 
 	//construct robot
 	ROBOT.left = port1;
-	ROBOT.right = port2;
+	ROBOT.right = port10;
 	ROBOT.lift1 = port3;
 	ROBOT.lift2 = port4;
 
 	while(true){
-	
+		
+		//if right button pressed
 		if(vexRT[Btn8R]==1){
-			x = x*-1;
+			//controls can be reversed
+			if(allowChange==1){
+				//reverse controls
+				x = x*-1;
+				//does not allow another reversal of controls while button is held
+				allowChange = 0;
+			}
 		}
-	
+		
+		else{
+			//controls can be reversed after releasing the button
+			allowChange = 1;
+		}
+
 		//move
-		if(abs(vexRT[Ch1])>20 || abs(vexRT[Ch3])>20){
-			go(ROBOT,vexRT[Ch3],vexRT[Ch1],x);
+		if(abs(vexRT[Ch2])>20 || abs(vexRT[Ch3])>20){
+			go(ROBOT,vexRT[Ch3],vexRT[Ch2],x);
 		}
 
 		//stop
 		else{
-			halt(ROBOT,x);
+			go(ROBOT,0,0,x);
 		}
 
 		//lift up
