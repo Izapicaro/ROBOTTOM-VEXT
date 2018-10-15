13652e4c036b81124af9a8eb6da694b7ce51d7f2
Izapicaro <34921842+Izapicaro@users.noreply.github.com>
2018-10-15 17:37:57 +0100

    Create Composite.c

---
+++ VCS Test/Composite.c
@@ -0,0 +1,70 @@
+#include "robot-config.h"
+          
+int main() {
+   
+    int x = 1;
+    int allowChange = 1;
+    int reapState = 0;
+    
+    while(true){
+        
+        // If button A is pressed
+        if(Controller.ButtonA.pressing()){
+            // If controls can be reversed
+            if(allowChange==1){
+                // Reverse controls
+                x=x*-1;
+                // Controls can no longer be reversed
+                allowChange = 0;
+            }
+        }
+        
+        // If button A is not pressed
+        else{
+            // Controls can now be reversed
+            allowChange = 1;
+        }
+        
+        // If button X is pressed
+        if(Controller.ButtonX.pressing()){
+            // Reap forwards
+            reapState = reapDirection(reapState);
+            reap(reapState);
+        }
+        
+        // If button B is pressed
+        else if(Controller.ButtonB.pressing()){
+            // Reap backwards
+            reapState = -reapDirection(reapState);
+            reap(reapState);
+        }
+        
+        // Move robot
+        if(abs(Controller.Axis3.value())>20 || abs(Controller.Axis2.value())>20){
+            go(x);
+        }
+       
+        // Stop robot
+        else{
+            go(0);
+        }
+        
+        // Raise lift
+        if(Controller.ButtonUp.pressing()){
+            lift(1);
+        }
+        
+        // Lower lift
+        else if(Controller.ButtonDown.pressing()){
+            lift(-1);
+        }
+        
+        // Stop lift
+        else{
+            lift(0);
+        }
+               
+        // Something about not wasting resources
+        vex::task::sleep(20);
+    }
+}
