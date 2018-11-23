2e61f891ec2b6ab5c4137a562cdf525855f3ccd7
Izapicaro <34921842+Izapicaro@users.noreply.github.com>
2018-11-23 18:22:28 +0000

    Update robot-config.h

--- VCS Test/robot-config.h
+++ VCS Test/robot-config.h
@@ -8,6 +8,9 @@ vex::motor Left1 (vex::PORT1, vex::gearSetting::ratio18_1, true);
 vex::motor Lift1 (vex::PORT2, vex::gearSetting::ratio18_1, false);
 vex::motor Lift2 (vex::PORT3, vex::gearSetting::ratio18_1, true);
 vex::motor Reaper (vex::PORT4, vex::gearSetting::ratio18_1, false);
+vex::motor Intaker (vex::PORT5, vex::gearSetting::ratio18_1, true);
+vex::motor Fw (vex::PORT6, vex::gearSetting::ratio18_1, true);
+vex::motor Fw2 (vex::PORT7, vex::gearSetting::ratio18_1, false);
 
 // Move tracks
 void go(int direction){
@@ -21,17 +24,19 @@ void lift(int direction){
     Lift2.spin(vex::directionType::fwd, 100*direction, vex::velocityUnits::pct);
 }
 
+// Flywheel
+void wheel(int direction){
+    Fw.spin(vex::directionType::fwd, 100*direction, vex::velocityUnits::pct);
+    Fw2.spin(vex::directionType::fwd, 100*direction, vex::velocityUnits::pct);
+}
+
 // Spin reaper
 void reap(int direction){
     Reaper.spin(vex::directionType::fwd, 100*direction, vex::velocityUnits::pct);
 }
 
-// Determine reap direction
-int reapDirection(int state){
-    if(abs(state)==1){
-        return 0;
-    }
-    else{
-        return 1;
-    }
+//Spin intake
+void intake(int power){
+    Intaker.spin(vex::directionType::fwd, 100*power, vex::velocityUnits::pct);
 }
+
